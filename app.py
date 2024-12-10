58d6e88a46af4ed3efae441e80422c61553339cf
seunghyeokleeme <seunghyeok.tech@gmail.com>
2024-12-11 06:52:21 +0900

    feat: GUI 기능 구현

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import json
 import random
 from datetime import datetime
+import tkinter.messagebox as msgbox
+from tkinter import *
+from tkinter import ttk
 
 TODO_DB = []
 TODO_FILE_PATH = 'todos.json'
@@ -16,26 +19,24 @@ def save_todo_db():
         with open(TODO_FILE_PATH, 'w') as file:
             json.dump(TODO_DB, file)
     except Exception as e:
-        print("DB에 저장할수 없습니다.", e)
-    else: 
-        print("DB에 저장되었습니다.")
+        msgbox.showerror("에러", "DB에 저장할수 없습니다.")
+        print("DB에 저장할 수 없습니다.", e)
+        
 
 def load_todo_db():
     try:
-        with open(TODO_FILE_PATH, 'r') as file:
+        with open(TODO_FILE_PATH, 'r', encoding='UTF-8') as file:
             TODO_DB.extend(json.load(file))
     except FileNotFoundError:
-        print("저장된 데이터가 없습니다.")
+        msgbox.showinfo("알림", "저장된 할 일이 없습니다.")
     except Exception as e:
+        msgbox.showerror("에러", "DB를 불러올 수 없습니다.")
         print("DB를 불러올 수 없습니다.", e)
     else:
-        print("DB를 불러왔습니다.")
+        msgbox.showinfo("알림", "DB를 성공적으로 불러왔습니다.")
     finally:
         print("DB를 불러오는 작업이 완료되었습니다.")
 
-def print_todo_details(id, todo):
-    priority_value = PRIORITY_LEVELS[todo['priority']]
-    print("{0}. [ ] {1} (기한: {2}, 우선순위: {3})".format(id, todo['title'], todo['due_date'], priority_value))
 
 def validate_priority(priority):
     if priority not in PRIORITY_LEVELS:
@@ -92,6 +93,7 @@ def add_todo(title, due_date, priority):
         validate_date(due_date)
         # 할 일 정보를 딕셔너리로 저장
         todo = {
+            'id': int(datetime.now().timestamp()),
             'title': title,
             'created_at': datetime.now().isoformat(),
             'due_date': due_date,
@@ -100,44 +102,45 @@ def add_todo(title, due_date, priority):
         TODO_DB.append(todo) # 할 일 정보를 TODO_DB에 추가
         save_todo_db() # DB에 저장
     except ValueError as e:
-        print(e)
-        print("올바른 데이터를 입력하세요")
-        return False
+        raise ValueError(e)
     else:
-        print('할 일이 추가되었습니다.')
         return True
 
-def get_todo(id):
+def get_todo(idx):
     todo_list = get_all_todos()
 
-    if id < 0 or id >= len(todo_list):
+    if idx < 0 or idx >= len(todo_list):
         raise IndexError('todo id is out of range')
 
-    return todo_list[id]
+    return todo_list[idx]
+
+def get_todo_index(id):
+    todo_list = get_all_todos()
+
+    for idx, todo in enumerate(todo_list):
+        if todo['id'] == id:
+            return idx
+    return -1
+
     
 # 할일 삭제 기능
-def delete_todo(id):
-    try:
-        todo_list = get_all_todos()
-        if id < 0 or id >= len(todo_list):
-            raise IndexError('todo id is out of range')
-        TODO_DB.pop(id)
-        save_todo_db()
-    except IndexError:
-        print("id 값이 올바르지 않습니다.")
-    else:
-        print('할 일 삭제 기능이 수행되었습니다.')
-        return True
+def delete_todo(idx):
+    todo_list = get_all_todos()
+    if idx < 0 or idx >= len(todo_list):
+        raise IndexError('todo id is out of range')
+    TODO_DB.pop(idx)
+    save_todo_db()
+    return True
 
 # 할일 수정 기능
-def update_todo(id, update_data):
+def update_todo(idx, update_data):
     try:
         validate_priority(update_data['priority'])
         validate_date(update_data['due_date'])
         todo_list = get_all_todos()
-        if id < 0 or id >= len(todo_list):
+        if idx < 0 or idx >= len(todo_list):
             raise IndexError('todo id is out of range')
-        todo = todo_list[id]
+        todo = todo_list[idx]
         
         for key in todo.keys():
             if key in update_data:
@@ -145,139 +148,225 @@ def update_todo(id, update_data):
         
         save_todo_db()
     except ValueError as e:
-        print(e)
-    except IndexError:
-        print("올바른 할 일 번호를 입력하세요")
-    except:
-        print("DB에 수정할 수 없습니다.")
+        raise ValueError(e)
     else:
-        print('할 일이 수정되었습니다.')
         return True
 
-def view_todos():
-    todo_list = get_all_todos()
-    if not todo_list:
-        print('등록된 할 일이 없습니다.')
-        return
-    
-    print("=== 전체 할 일 목록 ===")
-    for idx, todo in enumerate(todo_list):
-        print_todo_details(idx+1, todo)
-    print("===========================")
 
-def search_todos_by_priority():
-    priority = int(input("우선순위를 선택하세요. (높음: 0, 중간: 1, 낮음: 2): "))
-    filtered_todos = get_todos_by_priority(priority)
-    
-    # gui
-    if not filtered_todos:
-        print("해당 우선순위에 등록된 할 일이 없습니다.")
-        return
-    print("=== 우선순위가 {}인 할 일 목록 ===".format(PRIORITY_LEVELS[priority]))
-    for idx, todo in enumerate(filtered_todos):
-        print_todo_details(idx+1, todo)
-    print("===========================")
+def sorting_todos_by_priority():
+    sorted_todos = []
+    for priority in range(3):
+        todos = get_todos_by_priority(priority)
+        sorted_todos.extend(todos)
 
-def search_todos_by_date():
-    search_date = input('날짜를 입력하세요 (예 YYYY-MM-DD): ')
-    filtered_todos = get_todos_by_date(search_date)
+    todo_treeview.delete(*todo_treeview.get_children())
+    for _, todo in enumerate(sorted_todos):
+        priority_value = PRIORITY_LEVELS[todo['priority']]
+        todo_treeview.insert('', 'end', values=(todo['id'], todo['title'], todo['due_date'], priority_value))
 
-    # gui
-    if not filtered_todos:
-        print("해당 날짜에 등록된 할 일이 없습니다.")
-        return
-    print("=== 날짜가 {}인 할 일 목록 ===".format(search_date))
-    for idx, todo in enumerate(filtered_todos):
-        print_todo_details(idx+1, todo)
-    print("===========================")
 
-def create_todo_item():
+def sorting_todos_by_date():
+    sorted_todos = []
+    for todo in get_all_todos():
+        sorted_todos.append(todo)
+    sorted_todos.sort(key=lambda x: (x['due_date'], x['created_at']))
+
+    todo_treeview.delete(*todo_treeview.get_children())
+    for _, todo in enumerate(sorted_todos):
+        priority_value = PRIORITY_LEVELS[todo['priority']]
+        todo_treeview.insert('', 'end', values=(todo['id'], todo['title'], todo['due_date'], priority_value))
+
+def create_todo_handler():
     try:
-        title = input('할 일 제목을 입력하세요: ')
-        due_date = input('기한을 입력하세요 (예 YYYY-MM-DD): ')
-        priority = int(input('우선순위를 선택하세요. (높음: 0, 중간: 1, 낮음: 2): '))
+        title = todo_name_entry.get()
+        due_date = todo_due_date_entry.get()
+        priority = todo_priority_combobox.current()
             
         add_todo(title, due_date, priority)
-    except ValueError:
-        print("올바른 데이터를 입력하세요")
+    except ValueError as e:
+        msgbox.showwarning("경고", e)
+    else:
+        msgbox.showinfo("알림", "할 일이 추가되었습니다.")
+        populate_todo_treeview(todo_treeview)
+
 
 def delete_todo_item():
     try:
-        todo_id = int(input('삭제할 할 일 번호를 입력하세요: '))
-        delete_todo(todo_id)
-    except ValueError:
-        print("올바른 데이터를 입력하세요")
+        # treeview에서 선택된 아이템의 정보를 가져옴
+        selected_item = todo_treeview.selection()
+        if not selected_item:
+            msgbox.showwarning("경고", "삭제할 할 일을 선택하세요.")
+            return
+        todo_id = int(todo_treeview.item(selected_item, "values")[0])
+        idx = get_todo_index(todo_id)
+        delete_todo(idx)
+    except IndexError:
+        msgbox.showwarning("경고", "올바른 할 일 번호를 입력하세요")
+    except:
+        msgbox.showerror("에러", "할 일 삭제에 실패했습니다.")
+    else:
+        msgbox.showinfo("알림", "할 일이 삭제되었습니다.")
+        populate_todo_treeview(todo_treeview)
 
 def update_todo_item():
     try:
-        todo_id = int(input('수정할 할 일 번호를 입력하세요: '))
+        selected_item = todo_treeview.selection()
+        if not selected_item:
+            msgbox.showwarning("경고", "수정할 할 일을 선택하세요.")
+            return
+        todo_id = int(todo_treeview.item(selected_item, "values")[0])
         update_input_dto = {
-            'title': input('할 일 제목을 입력하세요: '),
-            'due_date': input('기한을 입력하세요 (예 YYYY-MM-DD): '),
-            'priority': int(input('우선순위를 선택하세요. (높음: 0, 중간: 1, 낮음: 2): '))
+            'title': todo_name_entry.get(),
+            'due_date': todo_due_date_entry.get(),
+            'priority': todo_priority_combobox.current()
         }
-        update_todo(todo_id, update_input_dto)
-    except ValueError:
-        print("올바른 데이터를 입력하세요")
+        idx = get_todo_index(todo_id)
+        update_todo(idx, update_input_dto)
+    except ValueError as e:
+        msgbox.showwarning("경고", e)
+    except Exception:
+        msgbox.showerror("에러", "할 일 수정에 실패했습니다.")
+    else:
+        msgbox.showinfo("알림", "할 일이 수정되었습니다.")
+        populate_todo_treeview(todo_treeview)
+
+def clear_todo_handler():
+    try:
+        TODO_DB.clear()
+        save_todo_db()
+    except:
+        msgbox.showerror("에러", "모든 할 일 삭제에 실패했습니다.")
+    else:
+        msgbox.showinfo("알림", "모든 할 일이 삭제되었습니다.")
+        populate_todo_treeview(todo_treeview)
 
 #랜덤모듈 활용 - 랜덤으로 할 일 추천받기
 def recommend_todo():
     todo_list = get_all_todos()
     if not todo_list:
-        print('등록된 할 일이 없습니다')
+        msgbox.showinfo("알림", "오늘은 휴식을 취하세요.")
         return
     random_todo = random.choice(todo_list)
-    print("=== 추천할 일 ===")
-    print("추천할 일: {}".format(random_todo['title']))
+    msgbox.showinfo("오늘의 할 일 추천", "오늘 할일 추천: {}".format(random_todo['title']))
+
+def populate_todo_treeview(treeview):
+    treeview.delete(*treeview.get_children())
+    for _, todo in enumerate(TODO_DB):
+        priority_value = PRIORITY_LEVELS[todo['priority']]
+        treeview.insert('', 'end', values=(todo['id'], todo['title'], todo['due_date'], priority_value))
 
 
-def display_menu():
-    print("===== To-Do List APP =====")
-    print("0. 종료")
-    print("1. 할 일 추가")
-    print("2. 할 일 수정")
-    print("3. 할 일 삭제")
-    print("4. 할 일 조회")
-    print("5. 날짜별 할 일 조회")
-    print("6. 우선순위별 할 일 조회")
-    print("7. 랜덤으로 할 일 추천받기")
-    print("===========================")
-
-def handle_todo_action(choice):
-    if choice == '1':
-        create_todo_item()
-    elif choice == '2':
-        update_todo_item()
-    elif choice == '3':
-        delete_todo_item()
-    elif choice == '4':
-        view_todos()
-    elif choice == '5':
-        search_todos_by_date()
-    elif choice == '6':
-        search_todos_by_priority()
-    elif choice == '7':
-        recommend_todo()
+def on_treeview_click(event):
+    try:
+        item = todo_treeview.selection()[0]
+        item_id = int(todo_treeview.item(item, "values")[0])
+        idx = get_todo_index(item_id)
+        todo = get_todo(idx)
+    except IndexError:
+        pass
     else:
-        print("올바른 메뉴 번호를 입력하세요(0-6)")
+        todo_name_entry.delete(0, END)
+        todo_name_entry.insert(0, todo['title'])
+        todo_due_date_entry.delete(0, END)
+        todo_due_date_entry.insert(0, todo['due_date'])
+        priority_value = PRIORITY_LEVELS[todo['priority']]
+        todo_priority_combobox.set(priority_value)
 
-def main():
-    load_todo_db()
-    while True:
-        try:
-            display_menu()
-            choice = input("원하는 작업을 선택하세요: ")
+def backup_handler():
+    try:
+        with open('backup.json', 'w') as file:
+            json.dump(TODO_DB, file)
+    except:
+        msgbox.showerror("에러", "백업에 실패했습니다.")
+    else:
+        msgbox.showinfo("알림", "백업이 완료되었습니다.")
 
-            if choice == '0':
-                print("프로그램을 종료합니다.")
-                save_todo_db()
-                break
-           
-            handle_todo_action(choice)
-        except Exception as e:
-            print("예외가 발생했습니다.", e)
-        finally:
-            print()
 
-if __name__ == '__main__':
-    main()
+
+window = Tk()
+window.title("To-Do List App")
+window.geometry("1024x768")
+
+form_frame = Frame(window, bg="lightgreen", width=200, height=200)
+form_frame.grid(row=0, column=0, rowspan=2, sticky=N+S)
+
+content_frame = Frame(window, bg="white")
+content_frame.grid(row=0, column=1, rowspan=2, sticky=N+S+E+W)
+
+nav_frame = Frame(window, height=50)
+nav_frame.grid(row=2, column=0, columnspan=2, sticky=E+W)
+
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(1, weight=1)
+
+todo_name_label = Label(form_frame, text="할 일 제목")
+todo_name_label.grid(row=0, column=0)
+
+todo_name_entry = Entry(form_frame)
+todo_name_entry.grid(row=0, column=1)
+
+todo_due_date_label = Label(form_frame, text="기한")
+todo_due_date_label.grid(row=1, column=0)
+
+todo_due_date_entry = Entry(form_frame)
+todo_due_date_entry.grid(row=1, column=1)
+
+todo_priority_label = Label(form_frame, text="우선순위")
+todo_priority_label.grid(row=2, column=0)
+
+todo_priority_combobox = ttk.Combobox(form_frame, values=list(PRIORITY_LEVELS.values()))
+todo_priority_combobox.grid(row=2, column=1)
+
+add_todo_btn = Button(nav_frame, text="할 일 추가", command=create_todo_handler)
+add_todo_btn.grid(row=0, column=0)
+
+update_todo_btn = Button(nav_frame, text="할 일 수정", command=update_todo_item)
+update_todo_btn.grid(row=0, column=1)
+
+delete_todo_btn = Button(nav_frame, text="할 일 삭제", command=delete_todo_item)
+delete_todo_btn.grid(row=0, column=2)
+
+recommend_todo_btn = Button(nav_frame, text="할 일 추천", command=recommend_todo)
+recommend_todo_btn.grid(row=0, column=3)
+
+delete_all_todo_btn = Button(nav_frame, text="모든 할 일 삭제", command=clear_todo_handler)
+delete_all_todo_btn.grid(row=0, column=4)
+
+exit_btn = Button(nav_frame, text="종료", command=window.quit)
+exit_btn.grid(row=0, column=5)
+
+backup_btn = Button(nav_frame, text="백업", command=backup_handler)
+backup_btn.grid(row=0, column=6)
+
+search_frame = Frame(content_frame)
+search_frame.pack()
+
+columns = ("ID", "제목", "기한", "우선순위")
+todo_treeview = ttk.Treeview(content_frame, columns=columns, show="headings")
+todo_treeview.heading("ID", text="ID")
+todo_treeview.heading("제목", text="제목")
+todo_treeview.heading("기한", text="기한")
+todo_treeview.heading("우선순위", text="우선순위")
+
+todo_treeview.pack(fill=BOTH, expand=True)
+
+search_option_label = Label(search_frame, text="정렬 옵션")
+search_option_label.grid(row=0, column=0)
+search_option_combobox = ttk.Combobox(search_frame, values=["우선순위", "날짜"])
+search_option_combobox.grid(row=0, column=1)
+
+def load_todos_based_on_sorting():
+    return sorting_todos_by_priority() if search_option_combobox.get() == "우선순위" else sorting_todos_by_date()
+
+search_btn = Button(search_frame, text="검색", command=lambda: load_todos_based_on_sorting())
+search_btn.grid(row=0, column=2)
+
+seacrh_all_btn = Button(search_frame, text="전체 할 일 조회", command=lambda: populate_todo_treeview(todo_treeview))
+seacrh_all_btn.grid(row=0, column=3)
+
+todo_treeview.bind("<ButtonRelease-1>", on_treeview_click)
+
+load_todo_db()
+populate_todo_treeview(todo_treeview)
+
+window.mainloop()
